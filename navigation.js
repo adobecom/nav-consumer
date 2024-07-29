109ff0ac1381e1f36331b0ae368799879a6b8aa7
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-29 15:12:13 +0530

    Updating default value for ims client id

--- navigation.js
+++ navigation.js
@@ -4,7 +4,7 @@ const privacyId = new URLSearchParams(window.location.search).get("privacyid");
 const locale = new URLSearchParams(window.location.search).get("locale");
 const navBranch = new URLSearchParams(window.location.search).get("navbranch") || "stage";
 const unavComponents = new URLSearchParams(window.location.search).get("unav") || "profile";
-const imsClientId = new URLSearchParams(window.location.search).get("imsclientid") || "fedsMilo";
+const imsClientId = new URLSearchParams(window.location.search).get("imsclientid") || "fedsmilo";
 
 async function init() {
   const { default: loadBlock } = await import(`https://${navBranch}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
