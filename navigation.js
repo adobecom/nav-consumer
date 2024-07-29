a46bbe3e9e091d299e2db8fca3c7db5e13b5b31a
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-29 12:27:19 +0530

    Updating test page with header

--- navigation.js
+++ navigation.js
@@ -1,11 +1,18 @@
-const authoringPath = new URLSearchParams(window.location.search).get('authoringpath') || '/federal/test-website';
-const env = new URLSearchParams(window.location.search).get('env') || 'prod';
-const privacyId = new URLSearchParams(window.location.search).get('privacyid');
-const locale = new URLSearchParams(window.location.search).get('locale');
-const navBranch = new URLSearchParams(window.location.search).get('navbranch') || 'main';
+const authoringPath = new URLSearchParams(window.location.search).get("authoringpath") || "/federal/dev";
+const env = new URLSearchParams(window.location.search).get("env") || "stage";
+const privacyId = new URLSearchParams(window.location.search).get("privacyid");
+const locale = new URLSearchParams(window.location.search).get("locale");
+const navBranch = new URLSearchParams(window.location.search).get("navbranch") || "gnav";
+const unavComponents = new URLSearchParams(window.location.search).get("unav") || "profile";
+const imsClientId = new URLSearchParams(window.location.search).get("imsclientid") || "fedsMilo";
 
 async function init() {
   const { default: loadBlock } = await import(`https://${navBranch}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
-  loadBlock({ env, locale, footer: { authoringPath, privacyId } });
+  loadBlock({
+    env,
+    locale,
+    footer: { authoringPath, privacyId },
+    header: { imsClientId, unavComponents },
+  });
 }
 init();
