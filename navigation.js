4385627cfd9d34f5a06903c1eb7f9ce26dba80df
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-30 18:41:35 +0530

    Adding js app for test

---
+++ navigation.js
@@ -0,0 +1,19 @@
+const authoringPath = new URLSearchParams(window.location.search).get("authoringpath") || "/federal/dev";
+const env = new URLSearchParams(window.location.search).get("env") || "stage";
+const privacyId = new URLSearchParams(window.location.search).get("privacyid");
+const locale = new URLSearchParams(window.location.search).get("locale");
+const navBranch = new URLSearchParams(window.location.search).get("navbranch") || "stage";
+const unavComponents = new URLSearchParams(window.location.search).get("unav") || "profile";
+const imsClientId = new URLSearchParams(window.location.search).get("imsclientid") || "fedsmilo";
+
+async function init() {
+  const { default: loadBlock } = await import(`https://${navBranch}--milo--adobecom.hlx.page/libs/navigation/navigation.js`);
+  loadBlock({
+    env,
+    locale,
+    authoringPath,
+    footer: { authoringPath, privacyId },
+    header: { imsClientId, unavComponents },
+  });
+}
+init();
